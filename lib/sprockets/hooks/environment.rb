326aa4929f83e7dd3bc33977c288390c64eb5707
Anton Zhuravsky <mail2lf@gmail.com>
2014-05-23 23:10:54 +0300

    Fixing 2.12.1 compatibility

--- lib/sprockets/hooks/environment.rb
+++ lib/sprockets/hooks/environment.rb
@@ -4,11 +4,14 @@ module Sprockets
   
     #  Returns asset instance trying to fetch it through virtual hash.
     #
-    def find_asset_with_virtuals(path, options = {})
+    def find_asset_with_virtuals(*args)
+      path    = args.first
+      options = args[1] || {}
+
       proc = SprocketsVirtualAssets.virtuals[path]
       return proc.call(path, self, options) if proc
       
-      find_asset_without_virtuals(path, options)
+      find_asset_without_virtuals(*args)
     end
   
     alias_method_chain :find_asset, :virtuals
